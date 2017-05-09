5d6bf85c17cf1fab95b76b954f22e0f1b34aba65
Erik Liland <erik.liland@gmail.com>
2017-05-09 18:20:28 +0200

    Removed commenting

--- pysimulator/analyzeResults.py
+++ pysimulator/analyzeResults.py
@@ -4,9 +4,9 @@ import multiprocessing as mp
 
 def analyseResults(filePathList=[], initFilePathList = []):
     with mp.Pool() as pool:
-        # pool.map(scenarioAnalyzer.analyzeTrackingFile, filePathList)
+        pool.map(scenarioAnalyzer.analyzeTrackingFile, filePathList)
         pool.map(scenarioAnalyzer.analyzeInitFile, initFilePathList)
 
 if __name__ == '__main__':
     from pysimulator.simulationConfig import trackingFilePathList, initFilePathList
-    analyseResults(trackingFilePathList, initFilePathList[0:1])
+    analyseResults(trackingFilePathList, initFilePathList)
