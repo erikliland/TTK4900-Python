0f1d400e8e7de4b1a8d1c3191f76b38b56594b46
Erik Liland <erik.liland@gmail.com>
2017-05-11 13:37:43 +0200

    Adapted to new pysimulator

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import pymht.utils.helpFunctions as hpf
 import pymht.models.pv as model
 from pysimulator import simulationConfig
 from pysimulator.scenarios.defaults import *
+import xml.etree.ElementTree as ET
+from pymht.utils.xmlDefinitions import *
 
 
 def runSimulation(**kwargs):
@@ -17,9 +19,9 @@ def runSimulation(**kwargs):
     avgToc = []
     tic0 = time.time()
 
-    scenario = simulationConfig.scenarioList[0]
+    scenario = simulationConfig.scenarioList[2]
 
-    seed = simulationConfig.baseSeed + 1
+    seed = simulationConfig.baseSeed + 2
     # p0 = np.array([100., -100.])
     # radarRange = 5500.0  # meters
     # maxSpeed = 22.0  # meters / second
@@ -30,42 +32,14 @@ def runSimulation(**kwargs):
     simTime = scenario.radarPeriod * 60  # sec
     # nScans = int(simTime / radarPeriod)
     # nSimulationSteps = int(simTime / simulationTimeStep)
-    lambda_phi = 4e-6
+    lambda_phi = 8e-6
     # lambda_nu = 0.0002
     P_d = 0.6  # Probability of detection
-    N = 1  # Number of  timesteps to tail (N-scan)
+    N = 6  # Number of  timesteps to tail (N-scan)
     # eta2 = 5.99  # 95% confidence
-    #
-    # assert simulationTimeStep <= radarPeriod
-    # assert simTime >= simulationTimeStep
-    # assert nScans >= 1
-    #
-    # sim.seed_simulator(seed)
-    # # nTargets = 4
-    # # meanSpeed = 10.0 * scipy.constants.knot  # meters/second
-    # # initialTargets = sim.generateInitialTargets(nTargets,p0, radarRange, meanSpeed)
-    # initTime = 0 # time.time()
-    # initialTargets = []
-    # initialTargets.append(SimTarget([-2000, 2100, 4, -4], initTime, P_d, model.sigmaQ_true))
-    # initialTargets.append(SimTarget([100, -2000, -2, 8], initTime, P_d, model.sigmaQ_true))
-    # initialTargets.append(SimTarget([-4000, 300, 12, -1], initTime, P_d, model.sigmaQ_true))
-    # initialTargets.append(SimTarget([-4000, 0, 12, 0], initTime, P_d, model.sigmaQ_true,
-    #                                 mmsi=257114401, aisClass='B', probabilityOfReceive=P_r))
-    # initialTargets.append(SimTarget([-4000, -200, 17, 1], initTime, P_d, model.sigmaQ_true))
-    # initialTargets.append(SimTarget([4000, -2000, 1, -8], initTime, P_d, model.sigmaQ_true))
-    # initialTargets.append(SimTarget([3000, 4000, 2, -8], initTime, P_d, model.sigmaQ_true))
-    # initialTargets.append(SimTarget([200, 5000, 10, -1], initTime, P_d, model.sigmaQ_true))
-    # initialTargets.append(SimTarget([-3500, -3500, 10, 5], initTime, P_d, model.sigmaQ_true))
-    # initialTargets.append(SimTarget([-4100, 3200, 17, 2], initTime, P_d, model.sigmaQ_true,
-    #                                 mmsi=257114400, aisClass='B', probabilityOfReceive=P_r))
-    # initialTargets.append(SimTarget([3600, 3000, -10, 3], initTime, P_d, model.sigmaQ_true))
-    # initialTargets.append(SimTarget([5000, 1000, -7, -2], initTime, P_d, model.sigmaQ_true))
-    # initialTargets.append(SimTarget([2000, 100, -10, 8], initTime, P_d, model.sigmaQ_true))
-    # initialTargets.append(SimTarget([0, -5000, 10, 2], initTime, P_d, model.sigmaQ_true))
-    # initialTargets.append(SimTarget([-400, 300, 17, 0], initTime, P_d, model.sigmaQ_true,
-    #                                 mmsi=257304900, aisClass='B', probabilityOfReceive=P_r))
-    # initialTargets.append(SimTarget([0, 2000, 15, 15], initTime, P_d, model.sigmaQ_true))
-
+    M_required = 2
+    N_checks = 3
+    preInitialized = True
 
     if kwargs.get('printInitialTargets', False):
         print("Initial targets:")
@@ -77,26 +51,8 @@ def runSimulation(**kwargs):
         print("Sim list:")
         print(*simList, sep="\n", end="\n\n")
 
-    scanList, aisList = scenario.getSimulatedScenario(seed, simList, lambda_phi, P_d)
-
-    # scanList = sim.simulateScans(simList,
-    #                              radarPeriod,
-    #                              model.C_RADAR,
-    #                              model.R_RADAR(model.sigmaR_RADAR_true),
-    #                              lambda_phi,
-    #                              radarRange,
-    #                              p0,
-    #                              localClutter=True,
-    #                              globalClutter=True,
-    #                              debug=False)
-    #
-    # aisList = sim.simulateAIS(simList,
-    #                           model.Phi,
-    #                           model.C_AIS,
-    #                           model.R_AIS(model.sigmaR_AIS_true),
-    #                           model.GPS_COVARIANCE_PRECISE,
-    #                           radarPeriod,
-    #                           probabilityOfReceive = P_r)
+    scanList, aisList = scenario.getSimulatedScenario(seed, simList, lambda_phi, P_d,
+                                                      localClutter=False)
 
     trackerArgs = (scenario.model,
                    scenario.radarPeriod,
@@ -114,17 +70,6 @@ def runSimulation(**kwargs):
 
     tracker = tomht.Tracker(*trackerArgs, groundTruth=simList, **{**trackerKwargs, **kwargs})
 
-    # tracker = tomht.Tracker(model.Phi(radarPeriod), model.C_RADAR, model.Gamma, P_d, model.P0,
-    #                         model.R_RADAR(), model.R_AIS(), model.Q(radarPeriod), lambda_phi, lambda_nu, eta2,
-    #                         p0, radarRange,
-    #                         N = N,
-    #                         logTime=True,
-    #                         logLevel=logging.DEBUG,
-    #                         maxSpeed=maxSpeed,
-    #                         radarPeriod = radarPeriod,
-    #                         M_required=2,
-    #                         N_checks=4,
-    #                         groundTruth=simList)
 
     toc0 = time.time() - tic0
     print("Generating simulation data for {0:} targets for {1:} seconds / {2:} scans.  It took {3:.1f} ms".format(
@@ -155,12 +100,12 @@ def runSimulation(**kwargs):
 
             tracker.addMeasurementList(measurementList,
                                        aisPredictions,
-                                       printTime=True,
+                                       printTime=False,
                                        printAssociation=False,
                                        printCluster=False,
-                                       checkIntegrity=True,
+                                       checkIntegrity=False,
                                        dynamicWindow=False,
-                                       pruneSimilar=True,
+                                       pruneSimilar=False,
                                        **kwargs)
             toc = time.time() - tic
             # print("H",tracker.__trackNodes__[7].measurementNumber,": ",tracker.__trackNodes__[7].cumulativeNLLR)
@@ -182,7 +127,7 @@ def runSimulation(**kwargs):
         p.strip_dirs().sort_stats('time').print_stats(20)
         p.strip_dirs().sort_stats('cumulative').print_stats(20)
     else:
-        simulate(tracker, simList, scanList, minToc, maxToc, avgToc, preInitialize=True)
+        simulate(tracker, simList, scanList, minToc, maxToc, avgToc, preInitialize=preInitialized)
 
     if kwargs.get('printTargetList', False):
         tracker.printTargetList()
@@ -198,10 +143,29 @@ def runSimulation(**kwargs):
 
     if 'exportPath' in kwargs:
         scenarioElement = tracker.getScenarioElement()
+        scenario.storeScenarioSettings(scenarioElement)
         simList.storeGroundTruth(scenarioElement, scenario)
-        tracker._storeRun(scenarioElement)
+        variationsElement = ET.SubElement(scenarioElement, variationsTag, attrib={preinitializedTag: str(preInitialized)})
+        variationDict = {pdTag: P_d,
+                         lambdaphiTag: lambda_phi,
+                         nTag: N}
+        variationElement = ET.SubElement(variationsElement,
+                                         variationTag,
+                                         attrib={str(k): str(v) for k, v in variationDict.items()})
+        tracker._storeRun(variationElement, preInitialized)
         path = kwargs.get('exportPath')
         hpf.writeElementToFile(path, scenarioElement)
+        print(path)
+        path1 = path if preInitialized else None
+        path2 = path if not preInitialized else None
+
+        from pysimulator import scenarioAnalyzer
+        scenarioAnalyzer.analyzeTrackingFile(path1)
+        scenarioAnalyzer.analyzeInitFile(path2)
+
+        from pysimulator import resultsPlotter
+        resultsPlotter.plotTrackingPercentage(path1)
+        resultsPlotter.plotInitializationTime(path2)
 
     if kwargs.get('plot'):
         import matplotlib.pyplot as plt
@@ -220,7 +184,7 @@ def runSimulation(**kwargs):
         # tracker.plotValidationRegionFromTracks() # TODO: Does not work
         desiredPlotPeriod = scenario.radarPeriod
         markEvery = max(1, int(desiredPlotPeriod / scenario.simulationTimeStep))
-        hpf.plotTrueTrack(simList, colors=colors1, markevery=markEvery, label=True)
+        hpf.plotTrueTrack(simList, colors=colors1, markevery=markEvery, label=False)
         # tracker.plotMeasurementsFromRoot(dummy=False, real = False, includeHistory=False)
         # tracker.plotStatesFromRoot(dummy=False, real=False, ais=True)
         # tracker.plotMeasurementsFromTracks(labels = False, dummy = True, real = True)
@@ -229,20 +193,17 @@ def runSimulation(**kwargs):
         # tracker.plotLastAisUpdate()
         # tracker.plotAllAisUpdates(stepsBack=N)
         # tracker.plotHypothesesTrack(colors=colors3, markStates=True)  # CAN BE SLOW!
-        tracker.plotActiveTracks(colors=colors2, markInitial=True, labelInitial=True, markRoot=True,
+        tracker.plotActiveTracks(colors=colors2, markInitial=True, labelInitial=True, markRoot=False,
                                  markStates=False, real=True, dummy=True, ais=True, smooth=False)
         # tracker.plotActiveTracks(colors=colors2, markInitial=False, markRoot=False, markStates=False, real=False,
         #                          dummy=False, ais=False, smooth=True, markEnd=False)
+        tracker.plotTerminatedTracks(markStates=False, real=False, dummy=False, ais=False, markInitial=True)
 
-        # tracker.plotTerminatedTracks(markStates=False, real=False, dummy=False, ais=True)
         plt.axis("equal")
         plt.xlim((scenario.p0[0] - scenario.radarRange * 1.05,
                   scenario.p0[0] + scenario.radarRange * 1.05))
         plt.ylim((scenario.p0[1] - scenario.radarRange * 1.05,
                   scenario.p0[1] + scenario.radarRange * 1.05))
-
-        # plt.xlim((-3910,-3870))
-        # plt.ylim((20,50))
         fig1.canvas.draw()
         plt.show()
 
@@ -261,7 +222,7 @@ if __name__ == '__main__':
     exportPath = os.path.join(os.path.expanduser(
         '~'), 'TTK4900-Python', 'data', 'test.xml')
     print("Storing at", exportPath)
-    runSimulation(plot=True,
+    runSimulation(plot=False,
                   profile=False,
                   printInitialTargets=False,
                   printTargetList=False,
