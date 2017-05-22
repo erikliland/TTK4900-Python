d906de6c6f6a0f39e8022f641036f44313ef5e22
Erik Liland <erik.liland@gmail.com>
2017-05-22 12:48:03 +0200

    Changed from 1.0 to .99 maximum probability of detection

--- pysimulator/simulationConfig.py
+++ pysimulator/simulationConfig.py
@@ -1,11 +1,11 @@
 import os
 from pysimulator.scenarios.scenarios import scenarioList
 
-pdList = [1., 0.8, 0.6]
+pdList = [0.99, 0.8, 0.6]
 lambdaphiList = [0, 2e-6, 4e-6, 8e-6]
 nList = [1, 3, 6, 9]
 path = os.path.join(os.path.expanduser('~'), 'TTK4900-Python', 'data')
-nMonteCarlo = 20
+nMonteCarlo = 10
 scenarioList = scenarioList
 trackingFilePathList = [os.path.join(path, scenario.name + "_Tracking" + ".xml") for scenario in scenarioList]
 initFilePathList = [os.path.join(path, scenario.name + "_Init" + ".xml") for scenario in scenarioList]
